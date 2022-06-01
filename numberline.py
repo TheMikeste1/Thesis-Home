4725f8aa8633f06e71cd1265c725c9b978f8f1cc
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-06-01 11:24:27 -0600

    Setup basic numberline

--- numberline.py
+++ numberline.py
@@ -5,7 +5,7 @@ import matplotlib.ticker as ticker
 import numpy as np
 
 
-def __setup(ax: plt.Axes, xmin: float, xmax: float):
+def __setup(ax: plt.Axes):
     # https://matplotlib.org/2.0.2/examples/ticks_and_spines/tick-locators.html
     ax.spines['right'].set_color('none')
     ax.spines['left'].set_color('none')
@@ -16,18 +16,23 @@ def __setup(ax: plt.Axes, xmin: float, xmax: float):
     ax.tick_params(which='major', length=5)
     ax.tick_params(which='minor', width=0.75)
     ax.tick_params(which='minor', length=2.5)
-    ax.set_xlim(xmin, xmax)
     ax.set_ylim(0, 1)
     ax.patch.set_alpha(0.0)
 
 
-def numberline(points: [], xmin, xmax, width=6) -> plt.Axes:
-    fig, ax = plt.subplots(1, 1)
-    fig.set_size_inches(width, 0.5)
+def numberline(points: [], xmin, xmax, ax=None, color=None) -> plt.Axes:
+    if ax is None:
+        fig, ax = plt.subplots()
 
-    __setup(ax, xmin, xmax)
+    __setup(ax)
+
     ax.xaxis.set_major_locator(ticker.AutoLocator())
     ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
-    ax.scatter(points, np.zeros(len(points)), clip_on=False, zorder=3)
+    ax.spines['bottom'].set_color('none')
+    ax.axis("scaled")
+    ax.set_xlim(xmin, xmax)
+
+    ax.scatter(points, np.zeros(len(points)), clip_on=False, zorder=3,
+               color=color)
 
     return ax
