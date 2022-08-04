5c2297cfa3dc6d7fc03b4f1816c0277cd53aee49
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-08-04 15:18:01 -0600

    Implemented Rankings

--- proxy_system/Rankings.py
+++ proxy_system/Rankings.py
@@ -1,2 +1,29 @@
+from typing import Iterable
+
+from proxy_system.RankingItem import RankingItem
+from proxy_system.TruthEstimator import TruthEstimator
+
+
 class Rankings:
-    pass
+    def __init__(self, rankings: Iterable[RankingItem] = None):
+        if rankings is None:
+            rankings = []
+        self.__rankings = {rank.rank: rank for rank in rankings}
+
+    def rankFor(self, agent: TruthEstimator) -> int:
+        for rank in self.__rankings.values():
+            if rank.proxy == agent:
+                return rank.rank
+        raise ValueError(f"Agent {agent} not in rankings")
+
+    def weightFor(self, agent: TruthEstimator) -> float:
+        return self.__rankings[self.rankFor(agent)].weight
+
+    def agentRanked(self, rank: int) -> TruthEstimator:
+        return self.__rankings[rank].proxy
+
+    def weightForRanked(self, rank: int) -> float:
+        return self.__rankings[rank].weight
+
+    def __iter__(self) -> Iterable[RankingItem]:
+        return sorted(self.__rankings.values(), key=lambda rank: rank.rank)
