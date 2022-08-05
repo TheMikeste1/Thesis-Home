5457781bbfc0dfbea55a638e90c73f1349fb6670
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-08-05 13:09:29 -0600

    Updated DistanceMechanism to use positive distances, as per report

--- proxy_estimate_system/weighting_mechanisms/distance_mechanism.py
+++ proxy_estimate_system/weighting_mechanisms/distance_mechanism.py
@@ -12,8 +12,11 @@ if TYPE_CHECKING:
 class DistanceMechanism(WeightingMechanism):
     def apply_weights(self, agent: TruthEstimator,
                       proxies: [TruthEstimator]) -> Rankings:
-        distances = {proxy: -abs(proxy.estimate - agent.estimate)
+        distances = {proxy: abs(proxy.estimate - agent.estimate)
                      for proxy in proxies}
+        max_distance = max(distances.values())
+        distances = {proxy: max_distance - dist
+                     for proxy, dist in distances.items()}
         ret = Rankings()
         for r, proxy in enumerate(
                 sorted(proxies, key=lambda p: distances[p]),
