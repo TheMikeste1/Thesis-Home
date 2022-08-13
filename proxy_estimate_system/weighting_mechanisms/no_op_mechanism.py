b314c16a7bc5bcb2b9224d9277e0febdb3b35572
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-08-13 12:02:08 -0400

    Fixed NoOp imports

--- proxy_estimate_system/weighting_mechanisms/no_op_mechanism.py
+++ proxy_estimate_system/weighting_mechanisms/no_op_mechanism.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from .weighting_mechanism import WeightingMechanism
-from .. import Rankings
+from ..rankings import Rankings
+
 
 if TYPE_CHECKING:
-    from proxy_estimate_system.truth_estimator import TruthEstimator
+    from proxy_estimate_system import TruthEstimator
 
 
 class NoOpMechanism(WeightingMechanism):
