3480f7f5d8d1335b98e09d4edc65833564942c88
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-08-13 13:13:59 -0400

    Fixed ending early

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import proxy_estimate_system as pes
 
 # %% Controls
 OUTPUT_DIR = "data"
-OUTPUT_INTERVAL = 100
+OUTPUT_INTERVAL = 1000
 
 MAX_PROXIES = 30
 MIN_PROXIES = 1
@@ -88,36 +88,32 @@ def get_dataframe_from_files(dir_with_files: str,
 
     files = os.listdir(dir_with_files)
     if verbose:
-        print(f"Checking {len(files)} files. . .")
+        log(f"Checking {len(files)} files. . .")
     data_files = []
     for i, file in enumerate(files, start=1):
         if verbose and i % 100 == 0:
-            print(
-                    f"Checking {i}/{len(files)} ("
-                    f"{i / len(files) * 100:.2f}%). . .")
+            log(f"Checking {i}/{len(files)} ("
+                f"{i / len(files) * 100:.2f}%). . .")
         path = f"{dir_with_files}{file}"
         if path.endswith(".feather") and os.path.isfile(path):
             data_files.append(path)
 
     if verbose:
-        print(f"Reading {len(data_files)} files. . .")
+        log(f"Reading {len(data_files)} files. . .")
     dfs = []
     for i, file in enumerate(data_files, start=1):
         if verbose and i % 100 == 0:
-            print(
-                    f"Reading {i}/{len(files)} ({i / len(files) * 100:.2f}%). "
-                    f". .")
+            log(f"Reading {i}/{len(files)} "
+                f"({i / len(files) * 100:.2f}%). . .")
         dfs.append(pd.read_feather(file))
-    print("Done reading! Concatenating. . .")
-    return pd.concat(dfs)
+    log("Done reading! Concatenating. . .")
+    return pd.concat(dfs).reset_index(drop=True)
 
 
 def perform_iterations():
     proxy_counts = list(range(MIN_PROXIES, MAX_PROXIES + 1, PROXY_STEP))
     inactive_counts = list(range(MIN_INACTIVE, MAX_INACTIVE + 1, INACTIVE_STEP))
-    combinations = product(proxy_counts, DISTRIBUTION_STRATEGIES, PROXY_EXTENTS,
-                           inactive_counts, DISTRIBUTION_STRATEGIES,
-                           INACTIVE_EXTENTS)
+
     paired_mechs = [
                        (v, "NoOp")
                        for v in WEIGHT_IGNORING_VMS
@@ -127,73 +123,78 @@ def perform_iterations():
                        set(VOTING_MECHANISMS) - set(WEIGHT_IGNORING_VMS)
                        for w in set(WEIGHTING_MECHANISMS) - {"NoOp"}
                    ]
-    total_combos = len(proxy_counts) * len(DISTRIBUTION_STRATEGIES) \
-                   * len(PROXY_EXTENTS) * len(inactive_counts) \
-                   * len(DISTRIBUTION_STRATEGIES) * len(INACTIVE_EXTENTS) \
-                   * len(paired_mechs)
+    non_vm_combos = len(proxy_counts) * len(DISTRIBUTION_STRATEGIES) \
+                    * len(PROXY_EXTENTS) * len(inactive_counts) \
+                    * len(DISTRIBUTION_STRATEGIES) * len(INACTIVE_EXTENTS)
+    total_combos = non_vm_combos * len(paired_mechs)
     total_start = datetime.datetime.now()
     it_start = total_start
     log(f"Performing {total_combos:,} combinations")
 
+    combinations = product(proxy_counts, DISTRIBUTION_STRATEGIES, PROXY_EXTENTS,
+                           inactive_counts, DISTRIBUTION_STRATEGIES,
+                           INACTIVE_EXTENTS, paired_mechs)
+
     # Keeping track of rows as a Python list and then converting to a DataFrame
     # is much faster than just using a DataFrame for some reason.
     rows = []
-    for voting_mechanism, inactive_weighting_mech in paired_mechs:
-        for i, (num_proxies, proxy_dist_strategy, proxy_extent,
-                num_inactive, inactive_dist_strategy, inactive_extent) \
-                in enumerate(combinations, start=1):
-            # Create system
-            proxies = [pes.Agent(
-                    DISTRIBUTION_STRATEGIES[proxy_dist_strategy](),
-                    proxy_extent)
-                for _ in range(num_proxies)]
-            inactive_voters = [pes.Agent(
-                    DISTRIBUTION_STRATEGIES[inactive_dist_strategy](),
-                    inactive_extent)
-                for _ in range(num_inactive)]
-            inactive_voters = [pes.InactiveVoter(
-                    a,
-                    WEIGHTING_MECHANISMS[inactive_weighting_mech]())
-                for a in inactive_voters]
-            vm = VOTING_MECHANISMS[voting_mechanism]()
-            system = pes.ProxySystem(proxies, inactive_voters, vm)
+    for i, (num_proxies, proxy_dist_strategy, proxy_extent,
+            num_inactive, inactive_dist_strategy, inactive_extent,
+            (voting_mechanism, inactive_weighting_mech)) \
+            in enumerate(combinations, start=1):
+        # Create system
+        proxies = [pes.Agent(
+                DISTRIBUTION_STRATEGIES[proxy_dist_strategy](),
+                proxy_extent)
+            for _ in range(num_proxies)]
+        inactive_voters = [pes.Agent(
+                DISTRIBUTION_STRATEGIES[inactive_dist_strategy](),
+                inactive_extent)
+            for _ in range(num_inactive)]
+        inactive_voters = [pes.InactiveVoter(
+                a,
+                WEIGHTING_MECHANISMS[inactive_weighting_mech]())
+            for a in inactive_voters]
+        vm = VOTING_MECHANISMS[voting_mechanism]()
+        system = pes.ProxySystem(proxies, inactive_voters, vm)
 
-            # Perform tests
-            for _ in range(NUM_ITERATIONS_PER_COMBO):
-                # run_seed = random.randint(0, 2 ** 64)
-                # system.set_seed(run_seed)
-                sout = system.estimate(0)
-                rows.append({
-                    "ProxyCount"                : num_proxies,
-                    "ProxyDistribution"         : proxy_dist_strategy,
-                    "ProxyExtent"               : proxy_extent,
-                    "InactiveCount"             : num_inactive,
-                    "InactiveDistribution"      : inactive_dist_strategy,
-                    "InactiveExtent"            : inactive_extent,
-                    "InactiveWeightingMechanism": inactive_weighting_mech,
-                    "VotingMechanism"           : voting_mechanism,
-                    "SystemEstimate"            : sout,
-                    "SquaredError"              : sout * sout,
-                    # "Seed"                      : run_seed,
-                })
+        # Perform tests
+        for _ in range(NUM_ITERATIONS_PER_COMBO):
+            # run_seed = random.randint(0, 2 ** 64)
+            # system.set_seed(run_seed)
+            sout = system.estimate(0)
+            rows.append({
+                "ProxyCount"                : num_proxies,
+                "ProxyDistribution"         : proxy_dist_strategy,
+                "ProxyExtent"               : proxy_extent,
+                "InactiveCount"             : num_inactive,
+                "InactiveDistribution"      : inactive_dist_strategy,
+                "InactiveExtent"            : inactive_extent,
+                "InactiveWeightingMechanism": inactive_weighting_mech,
+                "VotingMechanism"           : voting_mechanism,
+                "SystemEstimate"            : sout,
+                "SquaredError"              : sout * sout,
+                # "Seed"                      : run_seed,
+            })
 
-            # Print update
-            if i % OUTPUT_INTERVAL == 0:
-                # Output dataframe
-                if not os.path.exists(f"{OUTPUT_DIR}/tmp"):
-                    os.makedirs(f"{OUTPUT_DIR}/tmp")
-                df = pd.DataFrame(rows)
-                output_filename = f"PES" \
-                                  f"_{it_start.strftime('%d-%m-%Y_%H-%M-%S')}" \
-                                  f"_{len(df)}_rows"
-                df.to_feather(f"./{OUTPUT_DIR}/tmp/{output_filename}.feather")
-                rows = []
+        # Print update
+        if i % OUTPUT_INTERVAL == 0:
+            # Output dataframe
+            if not os.path.exists(f"{OUTPUT_DIR}/tmp"):
+                os.makedirs(f"{OUTPUT_DIR}/tmp")
+            df = pd.DataFrame(rows)
+            output_filename = f"PES" \
+                              f"_{it_start.strftime('%d-%m-%Y_%H-%M-%S')}" \
+                              f"_{len(df)}_rows"
+            df.to_feather(f"./{OUTPUT_DIR}/tmp/{output_filename}.feather")
+            rows = []
 
-                current_time = datetime.datetime.now()
-                log(f"{i:,}/{total_combos:,} ({i / total_combos * 100:.2f}%), "
-                    f"{current_time - it_start} since last update, "
-                    f"TOTAL: {current_time - total_start}")
-                it_start = current_time
+            current_time = datetime.datetime.now()
+            log(f"{i:,}/{total_combos:,} "
+                f"({i / total_combos * 100:.2f}%), "
+                f"{current_time - it_start} since last update, "
+                f"TOTAL: {current_time - total_start}")
+            it_start = current_time
 
     log(f"Combining results. . .")
     results = get_dataframe_from_files(f"{OUTPUT_DIR}/tmp")
