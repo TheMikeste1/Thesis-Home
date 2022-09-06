b2596ba7c3cd0540bdf35d1c8672a18fc050f580
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-09-06 11:43:44 -0600

    Black-ed code

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ PROXY_DISTRIBUTIONS = {
 }
 
 
-
 MAX_INACTIVE = 30
 MIN_INACTIVE = 1
 INACTIVE_STEP = 1
@@ -46,54 +45,38 @@ INACTIVE_DISTRIBUTIONS = {
 NUM_ITERATIONS_PER_COMBO = 20
 
 WEIGHTING_MECHANISMS = {
-    "Borda"      : pes.weighting_mechanisms.BordaMechanism,
-    "Closest"    : pes.weighting_mechanisms.ClosestMechanism,
-    "Distance"   : pes.weighting_mechanisms.DistanceMechanism,
+    "Borda": pes.weighting_mechanisms.BordaMechanism,
+    "Closest": pes.weighting_mechanisms.ClosestMechanism,
+    "Distance": pes.weighting_mechanisms.DistanceMechanism,
     "EqualWeight": pes.weighting_mechanisms.EqualWeightMechanism,
-    "NoOp"       : pes.weighting_mechanisms.NoOpMechanism,
+    "NoOp": pes.weighting_mechanisms.NoOpMechanism,
 }
 
 
 # %% System
 DISTRIBUTION_STRATEGIES = {
-    "Uniform"   : pes.distribution_strategies.UniformDistribution,
-    "Gaussian"  : pes.distribution_strategies.GaussianDistribution,
-    "Beta_.3_.3": partial(pes.distribution_strategies.BetaDistribution,
-                          a=0.3, b=0.3),
-    "Beta_4_4"  : partial(pes.distribution_strategies.BetaDistribution,
-                          a=4, b=4),
-    "Beta_4_1"  : partial(pes.distribution_strategies.BetaDistribution,
-                          a=4, b=1),
-    "Beta_3_.3" : partial(pes.distribution_strategies.BetaDistribution,
-                          a=3, b=0.3),
-    "Beta_1_4"  : partial(pes.distribution_strategies.BetaDistribution,
-                          a=1, b=4),
-    "Beta_.3_3" : partial(pes.distribution_strategies.BetaDistribution,
-                          a=0.3, b=3),
+    "Uniform": pes.distribution_strategies.UniformDistribution,
+    "Gaussian": pes.distribution_strategies.GaussianDistribution,
+    "Beta_.3_.3": partial(pes.distribution_strategies.BetaDistribution, a=0.3, b=0.3),
+    "Beta_4_4": partial(pes.distribution_strategies.BetaDistribution, a=4, b=4),
+    "Beta_4_1": partial(pes.distribution_strategies.BetaDistribution, a=4, b=1),
+    "Beta_3_.3": partial(pes.distribution_strategies.BetaDistribution, a=3, b=0.3),
+    "Beta_1_4": partial(pes.distribution_strategies.BetaDistribution, a=1, b=4),
+    "Beta_.3_3": partial(pes.distribution_strategies.BetaDistribution, a=0.3, b=3),
 }
 
 VOTING_MECHANISMS = {
     # Candidate
-    "InstantRunoffCandidate"        :
-        pes.voting_mechanisms.candidate.InstantRunoffMechanism,
-    "Median"                        :
-        pes.voting_mechanisms.candidate.MedianMechanism,
-    "Plurality"                     :
-        pes.voting_mechanisms.candidate.PluralityMechanism,
-    "WeightedInstantRunoffCandidate":
-        pes.voting_mechanisms.candidate.WeightedInstantRunoffMechanism,
-
+    "InstantRunoffCandidate": pes.voting_mechanisms.candidate.InstantRunoffMechanism,
+    "Median": pes.voting_mechanisms.candidate.MedianMechanism,
+    "Plurality": pes.voting_mechanisms.candidate.PluralityMechanism,
+    "WeightedInstantRunoffCandidate": pes.voting_mechanisms.candidate.WeightedInstantRunoffMechanism,
     # Average
-    "InstantRunoffAverage"          :
-        pes.voting_mechanisms.average.InstantRunoffMechanism,
-    "Mean"                          :
-        pes.voting_mechanisms.average.MeanMechanism,
-    "RankedChoice"                  :
-        pes.voting_mechanisms.average.RankedChoiceMechanism,
-    "WeightedInstantRunoffAverage"  :
-        pes.voting_mechanisms.average.WeightedInstantRunoffMechanism,
-    "WeightlessAverageAll"          :
-        pes.voting_mechanisms.average.WeightlessAverageAllMechanism,
+    "InstantRunoffAverage": pes.voting_mechanisms.average.InstantRunoffMechanism,
+    "Mean": pes.voting_mechanisms.average.MeanMechanism,
+    "RankedChoice": pes.voting_mechanisms.average.RankedChoiceMechanism,
+    "WeightedInstantRunoffAverage": pes.voting_mechanisms.average.WeightedInstantRunoffMechanism,
+    "WeightlessAverageAll": pes.voting_mechanisms.average.WeightlessAverageAllMechanism,
 }
 
 WEIGHT_IGNORING_VMS = {
@@ -104,17 +87,20 @@ WEIGHT_IGNORING_VMS = {
 # %% Functions
 def cleanup_output_dir(path: str):
     for file in os.listdir(path):
-        if file.startswith("PES") and \
-                file.endswith(".feather") and \
-                os.path.isfile(f"{path}/{file}"):
+        if (
+            file.startswith("PES")
+            and file.endswith(".feather")
+            and os.path.isfile(f"{path}/{file}")
+        ):
             os.remove(f"{path}/{file}")
     os.rmdir(path)
 
 
-def get_dataframe_from_files(dir_with_files: str,
-                             verbose: bool = False) -> pd.DataFrame:
-    if not dir_with_files.endswith('/'):
-        dir_with_files += '/'
+def get_dataframe_from_files(
+    dir_with_files: str, verbose: bool = False
+) -> pd.DataFrame:
+    if not dir_with_files.endswith("/"):
+        dir_with_files += "/"
 
     files = os.listdir(dir_with_files)
     if verbose:
@@ -130,8 +116,7 @@ def get_dataframe_from_files(dir_with_files: str,
     dfs = []
     for i, file in enumerate(data_files, start=1):
         if verbose and i % 100 == 0:
-            log(f"Reading {i}/{len(files)} "
-                f"({i / len(files) * 100:.2f}%). . .")
+            log(f"Reading {i}/{len(files)} " f"({i / len(files) * 100:.2f}%). . .")
         dfs.append(pd.read_feather(file))
     if verbose:
         log("Done reading! Concatenating. . .")
@@ -147,9 +132,7 @@ def output_df(df: pd.DataFrame, dir_: str):
     if not os.path.exists(dir_):
         os.makedirs(dir_)
     now = datetime.datetime.now()
-    output_filename = f"PES" \
-                      f"_{len(df)}_rows" \
-                      f"_{now.strftime('%d-%m-%Y_%H-%M-%S')}"
+    output_filename = f"PES" f"_{len(df)}_rows" f"_{now.strftime('%d-%m-%Y_%H-%M-%S')}"
     # Check it the file already exists so we don't overwrite it
     if os.path.exists(f"{dir_}/{output_filename}.feather"):
         i = 1
@@ -164,68 +147,83 @@ def perform_iterations():
     inactive_counts = list(range(MIN_INACTIVE, MAX_INACTIVE + 1, INACTIVE_STEP))
 
     paired_mechs = [
-                       (v, "NoOp")
-                       for v in
-                       set(VOTING_MECHANISMS) & set(WEIGHT_IGNORING_VMS)
-                   ] + [
-                       (v, w)
-                       for v in
-                       set(VOTING_MECHANISMS) - set(WEIGHT_IGNORING_VMS)
-                       for w in set(WEIGHTING_MECHANISMS) - {"NoOp"}
-                   ]
-    non_vm_combos = len(proxy_counts) * len(PROXY_DISTRIBUTIONS) \
-                    * len(PROXY_EXTENTS) * len(inactive_counts) \
-                    * len(INACTIVE_DISTRIBUTIONS) * len(INACTIVE_EXTENTS)
+        (v, "NoOp") for v in set(VOTING_MECHANISMS) & set(WEIGHT_IGNORING_VMS)
+    ] + [
+        (v, w)
+        for v in set(VOTING_MECHANISMS) - set(WEIGHT_IGNORING_VMS)
+        for w in set(WEIGHTING_MECHANISMS) - {"NoOp"}
+    ]
+    non_vm_combos = (
+        len(proxy_counts)
+        * len(PROXY_DISTRIBUTIONS)
+        * len(PROXY_EXTENTS)
+        * len(inactive_counts)
+        * len(INACTIVE_DISTRIBUTIONS)
+        * len(INACTIVE_EXTENTS)
+    )
     total_combos = non_vm_combos * len(paired_mechs)
     total_start = datetime.datetime.now()
     it_start = total_start
     log(f"Performing {total_combos:,} combinations")
 
-    combinations = product(proxy_counts, PROXY_DISTRIBUTIONS, PROXY_EXTENTS,
-                           inactive_counts, INACTIVE_DISTRIBUTIONS,
-                           INACTIVE_EXTENTS, paired_mechs)
+    combinations = product(
+        proxy_counts,
+        PROXY_DISTRIBUTIONS,
+        PROXY_EXTENTS,
+        inactive_counts,
+        INACTIVE_DISTRIBUTIONS,
+        INACTIVE_EXTENTS,
+        paired_mechs,
+    )
 
     tmp_directory = f"{OUTPUT_DIR}/tmp_{total_start.strftime('%d-%m-%Y_%H-%M-%S')}"
     # Keeping track of rows as a Python list and then converting to a DataFrame
     # is much faster than just using a DataFrame for some reason.
     rows = []
-    for i, (num_proxies, proxy_dist_strategy, proxy_extent,
-            num_inactive, inactive_dist_strategy, inactive_extent,
-            (voting_mechanism, inactive_weighting_mech)) \
-            in enumerate(combinations, start=1):
+    for i, (
+        num_proxies,
+        proxy_dist_strategy,
+        proxy_extent,
+        num_inactive,
+        inactive_dist_strategy,
+        inactive_extent,
+        (voting_mechanism, inactive_weighting_mech),
+    ) in enumerate(combinations, start=1):
 
         # Create system
-        proxies = [pes.Agent(
-                DISTRIBUTION_STRATEGIES[proxy_dist_strategy](),
-                proxy_extent)
-            for _ in range(num_proxies)]
-        inactive_voters = [pes.Agent(
-                DISTRIBUTION_STRATEGIES[inactive_dist_strategy](),
-                inactive_extent)
-            for _ in range(num_inactive)]
-        inactive_voters = [pes.InactiveVoter(
-                a,
-                WEIGHTING_MECHANISMS[inactive_weighting_mech]())
-            for a in inactive_voters]
+        proxies = [
+            pes.Agent(DISTRIBUTION_STRATEGIES[proxy_dist_strategy](), proxy_extent)
+            for _ in range(num_proxies)
+        ]
+        inactive_voters = [
+            pes.Agent(
+                DISTRIBUTION_STRATEGIES[inactive_dist_strategy](), inactive_extent
+            )
+            for _ in range(num_inactive)
+        ]
+        inactive_voters = [
+            pes.InactiveVoter(a, WEIGHTING_MECHANISMS[inactive_weighting_mech]())
+            for a in inactive_voters
+        ]
         vm = VOTING_MECHANISMS[voting_mechanism]()
         system = pes.ProxySystem(proxies, inactive_voters, vm)
 
         # Perform tests
         rows.extend(
-                {
-                    "ProxyCount"                : num_proxies,
-                    "ProxyDistribution"         : proxy_dist_strategy,
-                    "ProxyExtent"               : proxy_extent,
-                    "InactiveCount"             : num_inactive,
-                    "InactiveDistribution"      : inactive_dist_strategy,
-                    "InactiveExtent"            : inactive_extent,
-                    "InactiveWeightingMechanism": inactive_weighting_mech,
-                    "VotingMechanism"           : voting_mechanism,
-                    "SystemEstimate"            : sout,
-                    "SquaredError"              : sout * sout,
-                    # "Seed"                      : run_seed,
-                }
-                for sout in run_iterations(system, NUM_ITERATIONS_PER_COMBO)
+            {
+                "ProxyCount": num_proxies,
+                "ProxyDistribution": proxy_dist_strategy,
+                "ProxyExtent": proxy_extent,
+                "InactiveCount": num_inactive,
+                "InactiveDistribution": inactive_dist_strategy,
+                "InactiveExtent": inactive_extent,
+                "InactiveWeightingMechanism": inactive_weighting_mech,
+                "VotingMechanism": voting_mechanism,
+                "SystemEstimate": sout,
+                "SquaredError": sout * sout,
+                # "Seed"                      : run_seed,
+            }
+            for sout in run_iterations(system, NUM_ITERATIONS_PER_COMBO)
         )
 
         # Print update
@@ -236,10 +234,12 @@ def perform_iterations():
             rows = []
 
             current_time = datetime.datetime.now()
-            log(f"{i:,}/{total_combos:,} "
+            log(
+                f"{i:,}/{total_combos:,} "
                 f"({i / total_combos * 100:.2f}%), "
                 f"{current_time - it_start} since last update, "
-                f"TOTAL: {current_time - total_start}")
+                f"TOTAL: {current_time - total_start}"
+            )
             it_start = current_time
 
     # Output final dataframe
@@ -250,16 +250,22 @@ def perform_iterations():
         del rows
 
         current_time = datetime.datetime.now()
-        log(f"{total_combos:,}/{total_combos:,} "
+        log(
+            f"{total_combos:,}/{total_combos:,} "
             f"({100:.2f}%), "
             f"{current_time - it_start} since last update, "
-            f"TOTAL: {current_time - total_start}")
+            f"TOTAL: {current_time - total_start}"
+        )
 
     log(f"Combining results. . .")
     results = get_dataframe_from_files(tmp_directory, verbose=True)
     # Shrink categorical data types
-    for col in ["ProxyDistribution", "InactiveDistribution",
-                "InactiveWeightingMechanism", "VotingMechanism"]:
+    for col in [
+        "ProxyDistribution",
+        "InactiveDistribution",
+        "InactiveWeightingMechanism",
+        "VotingMechanism",
+    ]:
         results[col] = results[col].astype("category")
     output_df(results, f"{OUTPUT_DIR}")
 
@@ -272,10 +278,7 @@ def perform_iterations():
 
 
 def run_iterations(system: pes.ProxySystem, num_iterations: int) -> [int]:
-    output = [
-        system.estimate(0)
-        for _ in range(num_iterations)
-    ]
+    output = [system.estimate(0) for _ in range(num_iterations)]
     return output
 
 
--- numberline.py
+++ numberline.py
@@ -1,4 +1,4 @@
-__all__ = ['numberline']
+__all__ = ["numberline"]
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -7,15 +7,15 @@ import numpy as np
 
 def __setup(ax: plt.Axes):
     # https://matplotlib.org/2.0.2/examples/ticks_and_spines/tick-locators.html
-    ax.spines['right'].set_color('none')
-    ax.spines['left'].set_color('none')
+    ax.spines["right"].set_color("none")
+    ax.spines["left"].set_color("none")
     ax.yaxis.set_major_locator(ticker.NullLocator())
-    ax.spines['top'].set_color('none')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.tick_params(which='major', width=1.00)
-    ax.tick_params(which='major', length=5)
-    ax.tick_params(which='minor', width=0.75)
-    ax.tick_params(which='minor', length=2.5)
+    ax.spines["top"].set_color("none")
+    ax.xaxis.set_ticks_position("bottom")
+    ax.tick_params(which="major", width=1.00)
+    ax.tick_params(which="major", length=5)
+    ax.tick_params(which="minor", width=0.75)
+    ax.tick_params(which="minor", length=2.5)
     ax.set_ylim(0, 1)
     ax.patch.set_alpha(0.0)
 
@@ -28,11 +28,10 @@ def numberline(points: [], xmin, xmax, ax=None, color=None) -> plt.Axes:
 
     ax.xaxis.set_major_locator(ticker.AutoLocator())
     ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
-    ax.spines['bottom'].set_color('none')
+    ax.spines["bottom"].set_color("none")
     ax.axis("scaled")
     ax.set_xlim(xmin, xmax)
 
-    ax.scatter(points, np.zeros(len(points)), clip_on=False, zorder=3,
-               color=color)
+    ax.scatter(points, np.zeros(len(points)), clip_on=False, zorder=3, color=color)
 
     return ax
