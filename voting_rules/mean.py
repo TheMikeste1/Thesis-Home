2e9ed3b974f83365661388b764868e4d7a898c68
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-09-17 15:12:41 -0600

    Removed unused code

--- voting_rules/mean.py
+++
@@ -1,7 +0,0 @@
-def mean(values: []):
-    return sum(values) / len(values)
-
-
-def weighted_mean(weighted_values: [(float, float)]):
-    weights = [w for v, w in weighted_values]
-    return sum([v * w for v, w in weighted_values]) / sum(weights)
