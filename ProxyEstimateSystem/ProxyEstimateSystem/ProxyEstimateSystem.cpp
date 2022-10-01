db975663142267eff0534761b7c0bf9cea2a01cc
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-10-01 15:41:54 -0600

    CPP System start

---
+++ ProxyEstimateSystem/ProxyEstimateSystem/ProxyEstimateSystem.cpp
@@ -0,0 +1,12 @@
+﻿// ProxyEstimateSystem.cpp : Defines the entry point for the application.
+//
+
+#include "ProxyEstimateSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
