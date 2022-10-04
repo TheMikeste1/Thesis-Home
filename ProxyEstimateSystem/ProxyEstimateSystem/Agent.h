291898c43eff9f96b94b95ad572b61b7e5dd27f2
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-10-04 17:12:40 -0600

    Agent

--- ProxyEstimateSystem/ProxyEstimateSystem/Agent.h
+++ ProxyEstimateSystem/ProxyEstimateSystem/Agent.h
@@ -1,39 +1,48 @@
-/*
-from .distribution_strategies import DistributionStrategy
-from .truth_estimator import TruthEstimator
+#pragma once
 
+#include "TruthEstimator.h"
+#include "DistributionStrategies/DistributionStrategy.h"
 
-class Agent(TruthEstimator):
-    def __init__(self, distribution_strategy: DistributionStrategy,
-                 extent: float, ):
-        self.__distribution_strategy = distribution_strategy
-        self.__extent = extent
+class Agent : public TruthEstimator
+{
+private:
+   DistributionStrategy* _distributionStrategy;
+   double _extent;
 
-    @property
-    def distribution_strategy(self) -> DistributionStrategy:
-        return self.__distribution_strategy
+protected:
+   double _generateEstimate(double truth) override
+   {
+      return _distributionStrategy->getValue(truth - _extent, truth + _extent);
+   }
 
-    @distribution_strategy.setter
-    def distribution_strategy(self, value: DistributionStrategy):
-        self.__distribution_strategy = value
+public:
+   Agent(DistributionStrategy* distributionStrategy, double extent)
+         : _distributionStrategy(distributionStrategy), _extent(extent)
+   {
+   }
 
-    @property
-    def extent(self) -> float:
-        return self.__extent
+   DistributionStrategy* getDistributionStrategy() const
+   {
+      return _distributionStrategy;
+   }
 
-    @extent.setter
-    def extent(self, value: float):
-        self.__extent = value
+   void setDistributionStrategy(DistributionStrategy* distributionStrategy)
+   {
+      _distributionStrategy = distributionStrategy;
+   }
 
-    def set_seed(self, seed: int):
-        self.__distribution_strategy.set_seed(seed)
+   double getExtent() const
+   {
+      return _extent;
+   }
 
-    def _generate_estimate(self, truth: float) -> float:
-        return self.__distribution_strategy.get_value(truth - self.__extent,
-                                                      truth + self.__extent)
+   void setExtent(double extent)
+   {
+      _extent = extent;
+   }
 
-    def __str__(self):
-        return f'Agent(estimate={self.last_estimate} ' \
-               f'distribution_strategy={self.__distribution_strategy}, ' \
-               f'extent={self.__extent})'
-*/
+   void seed(unsigned int seed)
+   {
+      _distributionStrategy->seed(seed);
+   }
+};
