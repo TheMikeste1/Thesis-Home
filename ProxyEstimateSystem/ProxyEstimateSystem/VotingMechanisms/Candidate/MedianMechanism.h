6df1a3268124571e33bf371dc762cac65ba11758
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-10-04 12:04:50 -0600

    MedianMechanism

--- ProxyEstimateSystem/ProxyEstimateSystem/VotingMechanisms/Candidate/MedianMechanism.h
+++ ProxyEstimateSystem/ProxyEstimateSystem/VotingMechanisms/Candidate/MedianMechanism.h
@@ -1,31 +1,42 @@
-/*
-from __future__ import annotations
+#pragma once
 
-from typing import TYPE_CHECKING
+#include <algorithm>
+#include <map>
+#include <stdexcept>
 
-from ..voting_mechanism import VotingMechanism
+#include "../VotingMechanism.h"
 
-if TYPE_CHECKING:
-    from proxy_estimate_system import InactiveVoter, Rankings, TruthEstimator
+class MedianMechanism : public VotingMechanism
+{
+public:
+   double solve(
+         const std::vector<TruthEstimator*>& proxies,
+         const std::vector<InactiveVoter*>& inactive,
+         const std::map<InactiveVoter*, Rankings>& rankings
+   ) const override
+   {
+      auto* partitionedWeights = _sumProxyWeights(rankings);
 
-
-class MedianMechanism(VotingMechanism):
-    """
-    Gives the weighted median proxy's estimate, proxy whose weight sums with
-    those below it when sorted by estimate to is more the half the total
-    system weight.
-
-    When all proxies have the same weight, the median is simply the center
-    proxy when sorted by estimate.
-    """
-
-    def solve(self, proxies: [TruthEstimator], inactive: [InactiveVoter],
-              rankings: dict[InactiveVoter, Rankings]) -> float:
-        proxy_weights, system_weight = self._sum_proxy_weights(rankings)
-
-        sum_ = 0
-        for proxy in sorted(proxies, key=lambda x: x.last_estimate):
-            sum_ += proxy_weights[proxy]
-            if sum_ > system_weight / 2:
-                return proxy.last_estimate
-*/
+      std::vector<TruthEstimator*> sortedProxies(proxies);
+      std::sort(
+            sortedProxies.begin(),
+            sortedProxies.end(),
+            [](const auto& a, const auto& b) {
+               return a->getLastEstimate() < b->getLastEstimate();
+            }
+      );
+      double sum = 0;
+      for (const auto& proxy: sortedProxies)
+      {
+         sum += partitionedWeights->weights.at(proxy);
+         if (sum > partitionedWeights->systemWeight / 2)
+         {
+            delete partitionedWeights;
+            return proxy->getLastEstimate();
+         }
+      }
+      delete partitionedWeights;
+      // Should never reach here
+      throw std::runtime_error("MedianMechanism::solve() failed");
+   }
+};
