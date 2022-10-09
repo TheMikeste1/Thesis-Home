37af66863991439ce22d75a5083922fc0705736d
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-10-09 12:59:59 -0600

    Updated ArrowUtilities to save new data in DataRow

--- ProxyEstimateSystem/ProxyEstimateSystem/ArrowUtilities.cpp
+++ ProxyEstimateSystem/ProxyEstimateSystem/ArrowUtilities.cpp
@@ -13,11 +13,12 @@ arrow::Result<std::shared_ptr<arrow::Table>> vectorToColumnarTable(
 
    arrow::MemoryPool* pool = arrow::default_memory_pool();
 
-   arrow::DoubleBuilder errorBuilder(pool);
-   arrow::ListBuilder component_cost_builder(
-         pool,
-         std::make_shared<arrow::DoubleBuilder>(pool)
-   );
+   arrow::StringBuilder idBuilder(pool);
+   arrow::StringBuilder distributionBuilder(pool);
+   arrow::StringBuilder votingMechanismBuilder(pool);
+   arrow::Int32Builder numberOfProxiesBuilder(pool);
+   arrow::Int32Builder numberOfInactivesBuilder(pool);
+   arrow::DoubleBuilder estimateBuilder(pool);
 
    // Now we can loop over our existing data and insert it into the builders. The
    // `Append` calls here may fail (e.g. we cannot allocate enough additional memory).
@@ -25,16 +26,36 @@ arrow::Result<std::shared_ptr<arrow::Table>> vectorToColumnarTable(
    // check the documentation about `arrow::Status`.
    for (const auto& row: rows)
    {
-      ARROW_RETURN_NOT_OK(errorBuilder.Append(row.squaredError));
+      ARROW_RETURN_NOT_OK(idBuilder.Append(row.id));
+      ARROW_RETURN_NOT_OK(distributionBuilder.Append(row.distribution));
+      ARROW_RETURN_NOT_OK(votingMechanismBuilder.Append(row.votingMechanism));
+      ARROW_RETURN_NOT_OK(numberOfProxiesBuilder.Append(row.numberOfProxies));
+      ARROW_RETURN_NOT_OK(numberOfInactivesBuilder.Append(row.numberOfInactives));
+      ARROW_RETURN_NOT_OK(estimateBuilder.Append(row.estimate));
    }
 
    // At the end, we finalise the arrays, declare the (type) schema and combine them
    // into a single `arrow::Table`:
-   std::shared_ptr<arrow::Array> errorArray;
-   ARROW_RETURN_NOT_OK(errorBuilder.Finish(&errorArray));
+   std::shared_ptr<arrow::Array> idArray;
+   std::shared_ptr<arrow::Array> distributionArray;
+   std::shared_ptr<arrow::Array> votingMechanismArray;
+   std::shared_ptr<arrow::Array> numberOfProxiesArray;
+   std::shared_ptr<arrow::Array> numberOfInactivesArray;
+   std::shared_ptr<arrow::Array> estimatesArray;
+   ARROW_RETURN_NOT_OK(estimateBuilder.Finish(&idArray));
+   ARROW_RETURN_NOT_OK(estimateBuilder.Finish(&distributionArray));
+   ARROW_RETURN_NOT_OK(estimateBuilder.Finish(&votingMechanismArray));
+   ARROW_RETURN_NOT_OK(estimateBuilder.Finish(&numberOfProxiesArray));
+   ARROW_RETURN_NOT_OK(estimateBuilder.Finish(&numberOfInactivesArray));
+   ARROW_RETURN_NOT_OK(estimateBuilder.Finish(&estimatesArray));
 
    std::vector<std::shared_ptr<arrow::Field>> schemaVector = {
-         arrow::field("SquaredError", arrow::float64()),
+         arrow::field("id", arrow::utf8()),
+         arrow::field("Distribution", arrow::utf8()),
+         arrow::field("Voting Mechanism", arrow::utf8()),
+         arrow::field("Number of Proxies", arrow::int32()),
+         arrow::field("Number of Inactives", arrow::int32()),
+         arrow::field("Estimate", arrow::float64()),
    };
 
    auto schema = std::make_shared<arrow::Schema>(schemaVector);
@@ -43,7 +64,17 @@ arrow::Result<std::shared_ptr<arrow::Table>> vectorToColumnarTable(
    // that can consume Apache Arrow memory structures. This object has ownership of
    // all referenced data, thus we don't have to care about undefined references once
    // we leave the scope of the function building the table and its underlying arrays.
-   std::shared_ptr<arrow::Table> table = arrow::Table::Make(schema, {errorArray,});
+   std::shared_ptr<arrow::Table> table = arrow::Table::Make(
+         schema,
+         {
+               idArray,
+               distributionArray,
+               votingMechanismArray,
+               numberOfProxiesArray,
+               numberOfInactivesArray,
+               estimatesArray,
+         }
+   );
 
    return table;
 }
