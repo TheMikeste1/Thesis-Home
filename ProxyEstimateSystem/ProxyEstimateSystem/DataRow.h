ae6190e0e4ba4b997c9cab576d7d0fc74263d9d4
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-10-09 12:49:08 -0600

    Extended DataRow to include relevant information

--- ProxyEstimateSystem/ProxyEstimateSystem/DataRow.h
+++ ProxyEstimateSystem/ProxyEstimateSystem/DataRow.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <string>
+
 struct DataRow
 {
-   double squaredError;
+   std::string id;
+   std::string distribution;
+   std::string votingMechanism;
+   int numberOfProxies;
+   int numberOfInactives;
+   double estimate;
 };
